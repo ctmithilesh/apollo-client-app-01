00b34aa7f1354437a2386ce5af4a679cef25764c
ctmithilesh <mithilesht@cloudthat.com>
2023-05-22 13:00:53 +0530

    initial commit added multiple apollo servers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Employees from './Components/Employees';
+import Countries from './Components/Countries';
+import { ApolloProvider } from '@apollo/client'
+import one from './Clients/one'
+import two from './Clients/two'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h3> Data from Apollo Server - http://localhost:4000 </h3>
+      <ApolloProvider client={one}>
+          <Employees />
+      </ApolloProvider>
+      <h3> Data from Apollo Server - https://countries.trevorblades.com/ </h3>
+     <ApolloProvider client={two}>
+         <Countries />
+     </ApolloProvider>
+    </>
   );
 }
 
---
+++ src/Queries/getEmployees.js
@@ -0,0 +1,14 @@
+import { gql } from '@apollo/client'
+const GET_EMPLOYEES = gql`
+
+    query{
+        employees{
+            name,
+            dept 
+        }
+    }
+`
+export default GET_EMPLOYEES
+
+
+
