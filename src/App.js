46d59b201f16e2c377db36b8e3e35c39f1faf7b5
ctmithilesh <mithilesht@cloudthat.com>
2023-05-22 13:05:26 +0530

    added containers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import './App.css';
-import Employees from './Components/Employees';
+
 import Countries from './Components/Countries';
 import { ApolloProvider } from '@apollo/client'
-import one from './Clients/one'
 import two from './Clients/two'
+import EmployeePage from './Containers/EmployeePage';
+import CountriesPage from './Containers/CountriesPage';
 function App() {
   return (
     <>
-    <h3> Data from Apollo Server - http://localhost:4000 </h3>
-      <ApolloProvider client={one}>
-          <Employees />
-      </ApolloProvider>
-      <h3> Data from Apollo Server - https://countries.trevorblades.com/ </h3>
-     <ApolloProvider client={two}>
-         <Countries />
-     </ApolloProvider>
+      <EmployeePage />
+      <CountriesPage />
     </>
   );
 }
